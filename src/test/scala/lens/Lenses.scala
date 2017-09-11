fdc2da4c7ab7f8415968dcbbcd42862d7e74aa28
David Denton <denton.david@gmail.com>
2017-09-11 09:04:35 +0100

    started introducing lenses

--- src/test/scala/lens/Lenses.scala
+++ src/test/scala/lens/Lenses.scala
@@ -1,5 +1,19 @@
 package lens
 
+import shapeless.Lens
+
+/**
+  * Represents a uni-directional extraction of a list of entities from a target.
+  */
+trait MultiLensSpec[IN, OUT] {
+  def defaulted(name: String, default: List[OUT], description: String = null): Lens[IN, List[OUT]]
+
+  def optional(name: String, description: String = null): Lens[IN, Option[List[OUT]]]
+
+  def required(name: String, description: String = null): Lens[IN, List[OUT]]
+}
+
+
 class LensGet[IN, MID, OUT] private(private val rootFn: (String, IN) => List[MID], private val fn: (MID) => OUT) {
   def map[NEXT](nextFn: (OUT) => NEXT): LensGet[IN, MID, NEXT] = new LensGet[IN, MID, NEXT](rootFn, (i: MID) => nextFn(fn(i)))
 }
